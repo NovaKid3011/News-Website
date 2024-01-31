3560d72e730bc26e77a29dacc332a0b1112bd4fa
NovaKid3011 <158294843+NovaKid3011@users.noreply.github.com>
2024-01-31 22:50:13 +0800

    Add files via upload

--- database.php
+++ database.php
@@ -2,7 +2,7 @@
 
     $hostname = "localhost";
     $dbUser = "dfoiwidm_alonews";
-    $dbpassword = "Alonews12345";
+    $dbpassword = "Alonenews12345";
     $dbName = "dfoiwidm_alonews";
     $conn = mysqli_connect($hostname, $dbUser, $dbpassword, $dbName);
 
